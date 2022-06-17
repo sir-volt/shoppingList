628f34718d55fb77f94ee79a41d213c608cedaa7
Nicola Bartolucci <nicola.bartolucci2@studio.unibo.it>
2022-06-17 19:17:53 +0200

    Added basic database and user table implementation

---
+++ app/src/main/java/com/example/shoppinglist/DataBase/UserDatabase.java
@@ -0,0 +1,32 @@
+package com.example.shoppinglist.DataBase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.shoppinglist.UserEntity;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//Dovrei cambiargli il nome, userdatabase è un po' limitante dato che ci staranno anche gli articoli
+@Database(entities = {UserEntity.class}, version=1)
+public abstract class UserDatabase extends RoomDatabase {
+
+    private static final String dbName = "listappdatabase";
+    private static UserDatabase INSTANCE;
+    static final ExecutorService executor = Executors.newFixedThreadPool(4);
+
+    static UserDatabase getUserDatabase(final Context context){
+
+        if(INSTANCE == null){
+            synchronized (UserDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, dbName).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
--- app/src/main/java/com/example/shoppinglist/UserEntity.java
+++ app/src/main/java/com/example/shoppinglist/UserEntity.java
@@ -18,6 +18,36 @@ public class UserEntity {
 
     @ColumnInfo(name = "name")
     String name;
-    
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
