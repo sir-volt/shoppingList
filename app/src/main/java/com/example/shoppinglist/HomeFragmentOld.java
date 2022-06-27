12b2e00f14bf29b00ef7e92de6e67ef2f38ff883
Nicola Bartolucci <nicola.bartolucci2@studio.unibo.it>
2022-06-27 18:43:18 +0200

    Created new layouts for home screen (to be filled with lists cards) and temporarily renamed the old ones Old

---
+++ app/src/main/java/com/example/shoppinglist/HomeFragmentOld.java
@@ -0,0 +1,65 @@
+package com.example.shoppinglist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shoppinglist.RecyclerView.ListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragmentOld extends Fragment {
+
+    private ListAdapter adapter;
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_items_to_buy, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Activity activity = getActivity();
+        if(activity != null){
+            setRecyclerView(activity);
+
+            FloatingActionButton actionButton = view.findViewById(R.id.fab_add);
+            actionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Utilities.insertFragment((AppCompatActivity) activity,new AddFragment(), AddFragment.class.getSimpleName());
+                }
+            });
+
+        }else{
+            Log.e("HomeFragment","Activity is Null");
+        }
+
+    }
+
+    private void setRecyclerView(final Activity activity) {
+        recyclerView = activity.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        List<ListItem> itemList = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            itemList.add(new ListItem("ic_baseline_settings_24","generic Item",
+                    "Correct Price","Image Description"));
+        }
+        adapter = new ListAdapter(itemList, activity);
+        recyclerView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/shoppinglist/LoginFragment.java
+++ app/src/main/java/com/example/shoppinglist/LoginFragment.java
@@ -10,19 +10,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shoppinglist.DataBase.UserRepository;
-
-import java.util.List;
 
 public class LoginFragment extends Fragment {
 
