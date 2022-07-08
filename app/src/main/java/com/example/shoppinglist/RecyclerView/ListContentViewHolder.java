855b5aaa1bee8625b0d0473d30224f79e9e74666
Nicola Bartolucci <nicola.bartolucci2@studio.unibo.it>
2022-07-08 22:31:01 +0200

    Created Item in list card layout. Included it into list content xml. Created ListContentViewHolder

---
+++ app/src/main/java/com/example/shoppinglist/RecyclerView/ListContentViewHolder.java
@@ -0,0 +1,39 @@
+package com.example.shoppinglist.RecyclerView;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shoppinglist.R;
+
+public class ListContentViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    //TODO mettere i campi giusti
+    TextView listNameTextview;
+    TextView remainingTextview;
+
+    private final OnItemListener listener;
+
+    //VECCHIO COSTRUTTORE
+    /*public ShoppingListViewHolder(@NonNull View itemView) {
+        super(itemView);
+        listNameTextview = itemView.findViewById(R.id.list_name_textview);
+        remainingTextview = itemView.findViewById(R.id.remaining_textview);
+    }*/
+
+    //NUOVO COSTRUTTORE, SUPPORTA LISTENER
+    public ListContentViewHolder(@NonNull View itemView, OnItemListener listener) {
+        super(itemView);
+        listNameTextview = itemView.findViewById(R.id.list_name_textview);
+        remainingTextview = itemView.findViewById(R.id.remaining_textview);
+        this.listener = listener;
+        itemView.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        listener.onItemClick(getAdapterPosition());
+    }
+}
